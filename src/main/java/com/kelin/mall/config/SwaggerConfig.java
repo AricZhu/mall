b39903e2d00aa301558c022c13eb27d588227f2f
kelin.zrh <kelin.zrh@alibaba-inc.com>
2024-07-08 20:54:46 +0800

    添加 Swagger API 文档

--- src/main/java/com/kelin/mall/config/SwaggerConfig.java
+++ src/main/java/com/kelin/mall/config/SwaggerConfig.java
@@ -1,10 +1,29 @@
 package com.kelin.mall.config;
 
 import com.kelin.mall.domain.SwaggerProperties;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebFluxRequestHandlerProvider;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Swagger API文档相关配置
@@ -12,23 +31,58 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends BaseSwaggerConfig {
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        Docket docket = new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo()).enable(true)
+                .select()
+                //apis： 添加swagger接口提取范围
+                .apis(RequestHandlerSelectors.basePackage("com.kelin.mall.controller"))
+                .paths(PathSelectors.any())
+                .build();
 
-    @Override
-    public SwaggerProperties swaggerProperties() {
-        return SwaggerProperties.builder()
-                .apiBasePackage("com.macro.mall.demo.controller")
-                .title("mall-demo系统")
-                .description("SpringBoot版本中的一些示例")
-                .contactName("macro")
-                .version("1.0")
-                .enableSecurity(true)
+        return docket;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API 文档")
+                .description("API 在线文档")
+                .version("v1.0")
                 .build();
     }
 
     @Bean
-    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-        return generateBeanPostProcessor();
-    }
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
 
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream()
+                        .filter(mapping -> mapping.getPatternParser() == null)
+                        .collect(Collectors.toList());
+                mappings.clear();
+                mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
 }
--- src/main/java/com/kelin/mall/controller/PmsBrandController.java
+++ src/main/java/com/kelin/mall/controller/PmsBrandController.java
@@ -4,6 +4,8 @@ import com.kelin.mall.common.api.CommonPage;
 import com.kelin.mall.common.api.CommonResult;
 import com.kelin.mall.mbg.model.PmsBrand;
 import com.kelin.mall.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
 @Controller
 @RequestMapping("/brand")
 public class PmsBrandController {
@@ -20,12 +23,14 @@ public class PmsBrandController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
+    @ApiOperation("获取所有品牌列表")
     @RequestMapping(value = "listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<PmsBrand>> getBrandList() {
         return CommonResult.success(brandService.listAllBrand());
     }
 
+    @ApiOperation("添加品牌")
     @RequestMapping(value = "create", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult createBand(@RequestBody PmsBrand pmsBrand) {
@@ -38,6 +43,7 @@ public class PmsBrandController {
         return CommonResult.failed("创建失败！");
     }
 
+    @ApiOperation("更新指定id品牌信息")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult updateBrand(@PathVariable("id") Long id , @RequestBody PmsBrand pmsBrandDto) {
@@ -50,6 +56,7 @@ public class PmsBrandController {
         return CommonResult.failed("更新失败！");
     }
 
+    @ApiOperation("删除指定id的品牌")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult deleteBrand(@PathVariable("id") Long id) {
@@ -62,6 +69,7 @@ public class PmsBrandController {
         return CommonResult.failed("删除失败！");
     }
 
+    @ApiOperation("分页查询品牌列表")
     @RequestMapping(value = "list", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize") Integer pageSize) {
@@ -69,6 +77,7 @@ public class PmsBrandController {
         return CommonResult.success(CommonPage.restPage(pmsBrands));
     }
 
+    @ApiOperation("获取指定id的品牌详情")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<PmsBrand> brand(@PathVariable("id") Long id) {
